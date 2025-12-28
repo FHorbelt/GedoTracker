d079d9c1671aae669abcddd8fab751efbc90bd93
Florian Horbelt <horbelt.florian@web.de>
2025-12-28 14:35:08 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/GedoTracker/sw.js', { scope: '/GedoTracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
